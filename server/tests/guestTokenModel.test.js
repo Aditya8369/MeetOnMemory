3d958227ba687c6522eb71a5d62689dc6069f700
Aryanbuha890 <aryan.buha89@gmail.com>
2026-08-21 23:13:47 +0530

    feat: implement GuestToken model and GuestJoin landing portal view

---
+++ server/tests/guestTokenModel.test.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const { default: GuestToken } = await import("../models/GuestToken.js");
+
+describe("GuestToken Model Schema Definition Tests (#1900)", () => {
+  it("compiles and exports GuestToken successfully", () => {
+    expect(GuestToken).toBeDefined();
+    expect(GuestToken.modelName).toBe("GuestAccessToken");
+  });
+});
