5b07a2f5c4d22a82443f6b7f79f4dc3fd82b1dcd
Himanshu Raj <156138261+ErebAsh@users.noreply.github.com>
2026-08-21 22:15:38 +0530

    feat: add Meeting Minutes (MoM) approval workflow

--- server/routes/index.js
+++ server/routes/index.js
@@ -91,6 +91,7 @@ import breakoutRoomRoutes from "./breakoutRoomRoutes.js";
 import notionIntegrationRoutes from "./notionIntegrationRoutes.js";
 import gamificationRoutes from "./gamificationRoutes.js";
 import actionItemsRoutes from "./actionItems.routes.js";
+import minutesApprovalRoutes from "./minutesApprovalRoutes.js";
 
 const router = express.Router();
 
@@ -149,6 +150,7 @@ router.use("/api/polls", pollRoutes);
 router.use("/api/meetings/:meetingId/topics", topicRoutes);
 router.use("/api/meetings/:meetingId/breakout-rooms", breakoutRoomRoutes);
 router.use("/api/meetings/:meetingId/attachments", attachmentRoutes);
+router.use("/api/meetings/:meetingId/minutes-approval", minutesApprovalRoutes);
 router.use("/api/meeting-series", meetingSeriesRoutes);
 router.use("/api/meeting-series", carryForwardRoutes);
 router.use("/api/comparison", comparisonRoutes);
