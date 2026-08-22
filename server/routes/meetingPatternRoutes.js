5d57b6806f2f8ccb8b52e0e2fe9d43025e4a80b6
Babin123456 <babin.elusoc2026@gmail.com>
2026-08-23 00:56:08 +0530

    feat: actionable meeting patterns with task generation and automation rules (#2068)

--- server/routes/meetingPatternRoutes.js
+++ server/routes/meetingPatternRoutes.js
@@ -4,6 +4,8 @@ import {
   acknowledgePattern,
   dismissPattern,
   triggerManualScan,
+  createTaskFromPattern,
+  configureAutomationFromPattern,
 } from "../controllers/meetingPatternController.js";
 import userAuth from "../middleware/userAuth.js";
 import { requireAdminOrOwner } from "../middleware/rbac.js";
@@ -15,6 +17,10 @@ router.use(userAuth);
 router.get("/", getPatterns);
 router.patch("/:id/acknowledge", acknowledgePattern);
 router.patch("/:id/dismiss", dismissPattern);
+
+// Pattern actionability (Task generation & Automation rule configuration)
+router.post("/:id/create-task", createTaskFromPattern);
+router.post("/:id/configure-automation", configureAutomationFromPattern);
 
 // Admin only routes
 router.post("/scan", requireAdminOrOwner, triggerManualScan);
