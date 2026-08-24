dbe87b20df28605255b2e18d43912f041c7dcfbe
prathvik mehra <prathvik.mehra.cg@gmail.com>
2026-08-24 14:13:09 +0530

    feat: Implement semantic knowledge graph extraction and neighborhood querying

--- server/routes/index.js
+++ server/routes/index.js
@@ -271,4 +271,7 @@ router.use("/api/observers", observerRoutes);
 import skillEndorsementRoutes from "./skillEndorsementRoutes.js";
 router.use("/api/skill-endorsements", skillEndorsementRoutes);
 
+import semanticKnowledgeGraphRoutes from "./semanticKnowledgeGraphRoutes.js";
+router.use("/api/semantic-graph", semanticKnowledgeGraphRoutes);
+
 export default router;
